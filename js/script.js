48b4d2bd6815448c6e3578ea505f72c081eb69a3
sserges <agounkeserge@gmail.com>
2022-08-18 18:24:17 -0400

    Create Save Button

--- js/script.js
+++ js/script.js
@@ -18,6 +18,11 @@ const onSubmit = (e) => {
       hideSpinner()
 
       generateQRCode(url, size)
+
+      setTimeout(() => {
+        const saveUrl = qr.querySelector('img').src
+        createSaveBtn(saveUrl)
+      })
     }, 1000)
   }
 }
@@ -40,6 +45,17 @@ const hideSpinner = () => {
 
 const clearUI = () => {
   qr.innerHTML = ''
+}
+
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement('a')
+  link.id = 'save-link'
+  link.classList =
+    'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5'
+  link.href = saveUrl
+  link.download = 'qrcode'
+  link.innerText = 'Save Image'
+  document.getElementById('generated').appendChild(link)
 }
 
 hideSpinner()
